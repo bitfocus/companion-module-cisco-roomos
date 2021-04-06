6ebb087a98e09321f61562f7191e0d9400fd4af8
JeffreyDavidsz <jeffrey.davidsz@vicreo.eu>
2021-04-06 23:26:37 +0200

    Add a lot of actions

--- src/actions.ts
+++ src/actions.ts
@@ -18,7 +18,19 @@ export enum ActionId {
 	MicrophoneNoiseRemoval = 'microphones_noise_removal',
 	Presentation = 'presentation',
 	VideoMatrix = 'video_matrix',
-	CameraPreset = 'camera_preset'
+	CameraPreset = 'camera_preset',
+	SetMainVideoSource = 'set_main_video_source',
+	CameraPositionSet = 'camera_position_set',
+	VideoMatrixReset = 'video_matrix_reset',
+	TriggerAutofocus = 'trigger_auto_focus',
+	CameraRamp = 'camera_ramp',
+	DTMFSend = 'DTMF_send',
+	ConferenceDoNotDisturbActivate = 'conference_do_not_disturb_activate',
+	ConferenceDoNotDisturbDeActivate = 'conference_do_not_disturb_deactivate',
+	SelfView = 'self_view',
+	OSDKeyClick = 'OSD_key_click',
+	OSDKeyPress = 'OSD_key_press',
+	OSDKeyRelease = 'OSD_key_release'
 }
 
 function WebexOnOffBooleanDropdown(id: string, label: string): CompanionInputFieldDropdown {
@@ -36,6 +48,60 @@ function WebexOnOffBooleanDropdown(id: string, label: string): CompanionInputFie
 				label: 'Off',
 				id: 'Off'
 			}
+		]
+	}
+}
+function RemoteKeyToPressDropdown(): CompanionInputFieldDropdown {
+	return {
+		id : 'Key',
+		label: 'Key',
+		type: 'dropdown',
+		default: 'Home',
+		choices: [
+			{	label: '0', id: '0'	},
+			{	label: '1', id: '1'	},
+			{	label: '2', id: '2'	},
+			{	label: '3', id: '3'	},
+			{	label: '4', id: '4'	},
+			{	label: '5', id: '5'	},
+			{	label: '6', id: '6'	},
+			{	label: '7', id: '7'	},
+			{	label: '8', id: '8'	},
+			{	label: '9', id: '9'	},
+			{	label: 'C', id: 'C'	},
+			{	label: 'Call', id: 'Call'	},
+			{	label: 'Disconnect', id: 'Disconnect'	},
+			{	label: 'Down', id: 'Down'	},
+			{	label: 'F1', id: 'F1'	},
+			{	label: 'F2', id: 'F2'	},
+			{	label: 'F3', id: 'F3'	},
+			{	label: 'F4', id: 'F4'	},
+			{	label: 'F5', id: 'F5'	},
+			{	label: 'Grab', id: 'Grab'	},
+			{	label: 'Home', id: 'Home'	},
+			{	label: 'Layout', id: 'Layout'	},
+			{	label: 'Left', id: 'Left'	},
+			{	label: 'Mute', id: 'Mute'	},
+			{	label: 'MuteMic', id: 'MuteMic'	},
+			{	label: 'Ok', id: 'Ok'	},
+			{	label: 'PhoneBook', id: 'PhoneBook'	},
+			{	label: 'Presentation', id: 'Presentation'	},
+			{	label: 'Right', id: 'Right'	},
+			{	label: 'Selfview', id: 'Selfview'	},
+			{	label: 'Square', id: 'Square'	},
+			{	label: 'SrcAux', id: 'SrcAux'	},
+			{	label: 'SrcCamera', id: 'SrcCamera'	},
+			{	label: 'SrcDocCam', id: 'SrcDocCam'	},
+			{	label: 'SrcPc', id: 'SrcPc'	},
+			{	label: 'SrcVcr', id: 'SrcVcr'	},
+			{	label: 'Star', id: 'Star'	},
+			{	label: 'Up', id: 'Up'	},
+			{	label: 'VolumeUp', id: 'VolumeUp'	},
+			{	label: 'VolumeDown', id: 'VolumeDown'	},
+			{	label: 'ZoomIn', id: 'ZoomIn'	},
+			{	label: 'ZoomOut', id: 'ZoomOut'	},
+			{	label: '+', id: '+'	},
+			{	label: '-', id: '-'	}
 		]
 	}
 }
@@ -76,7 +142,16 @@ export function GetActionsList(self: WebexInstanceSkel<DeviceConfig>): Companion
 	}
 	actions[ActionId.Disconnect] = {
 		label: 'Call: Disconnect',
-		options: []
+		options: [
+			{
+				label: 'Call ID 0 = All',
+				id: 'CallId',
+				type: 'number',
+				min: 0,
+				max: 255,
+				default: 0
+			}
+		]
 	}
 	actions[ActionId.Accept] = {
 		label: 'Call: Accept all incoming calls',
@@ -205,6 +280,55 @@ export function GetActionsList(self: WebexInstanceSkel<DeviceConfig>): Companion
 			}
 		]
 	}
+	actions[ActionId.VideoMatrix] = {
+		label: 'Video Matrix',
+		options: [
+			{
+				label: 'Layout',
+				id: 'Layout',
+				type: 'dropdown',
+				choices: [
+					{ id: 'Equal', label: 'Equal' },
+					{ id: 'Prominent', label: 'Prominent' }
+				],
+				default: 'Equal'
+			},
+			{
+				label: 'Mode',
+				id: 'Mode',
+				type: 'dropdown',
+				choices: [
+					{ id: 'Add', label: 'Add' },
+					{ id: 'Replace', label: 'Replace' }
+				],
+				default: 'Add'
+			},
+			{
+				label: 'Output',
+				id: 'Output',
+				default: 1,
+				type: 'number',
+				min: 1,
+				max: 3
+			},
+			{
+				label: 'RemoteMain',
+				id: 'RemoteMain',
+				default: 1,
+				type: 'number',
+				min: 1,
+				max: 4
+			},
+			{
+				label: 'SourceId',
+				id: 'SourceId',
+				default: 1,
+				type: 'number',
+				min: 1,
+				max: 255
+			}
+		]
+	}
 	actions[ActionId.CameraPreset] = {
 		label: 'Camera: Activate Camera preset',
 		options: [
@@ -217,6 +341,289 @@ export function GetActionsList(self: WebexInstanceSkel<DeviceConfig>): Companion
 				max: 35
 			}
 		]
+	}
+	actions[ActionId.SetMainVideoSource] = {
+		label: 'Set Main Video Source',
+		options: [
+			{
+				label: 'ConnectorId',
+				id: 'ConnectorId',
+				default: 1,
+				type: 'number',
+				min: 1,
+				max: 255
+			},
+			{
+				label: 'Layout',
+				id: 'Layout',
+				type: 'dropdown',
+				choices: [
+					{ id: 'Equal', label: 'Equal' },
+					{ id: 'PIP', label: 'PIP' },
+					{ id: 'Prominent', label: 'Prominent' }
+				],
+				default: 'Equal'
+			},
+			{
+				label: 'PIP Position',
+				id: 'PIPPosition',
+				type: 'dropdown',
+				choices: [
+					{ id: 'LowerLeft', label: 'LowerLeft' },
+					{ id: 'LowerRight', label: 'LowerRight' },
+					{ id: 'UpperLeft', label: 'UpperLeft' },
+					{ id: 'UpperRight', label: 'UpperRight' }
+				],
+				default: 'LowerLeft'
+			},
+			{
+				label: 'PIP Size',
+				id: 'PIPSize',
+				type: 'dropdown',
+				choices: [
+					{ id: 'Auto', label: 'Auto' },
+					{ id: 'Large', label: 'Large' }
+				],
+				default: 'Auto'
+			},
+			{
+				label: 'SourceId',
+				id: 'SourceId',
+				default: 1,
+				type: 'number',
+				min: 1,
+				max: 255
+			}
+		]
+	}
+	actions[ActionId.CameraPositionSet] = {
+		label: 'Set camera position',
+		options: [
+			{
+				label: 'CameraId',
+				type: 'number',
+				min: 1,
+				max: 7,
+				default: 1,
+				id: 'CameraId'
+			},
+			{
+				label: 'Focus',
+				type: 'number',
+				min: 0,
+				max: 65535,
+				default: 30000,
+				id: 'Focus'
+			},
+			{
+				label: 'Lens',
+				type: 'dropdown',
+				choices: [{label: 'Wide', id: 'Wide'},{label: 'Center', id: 'Center'},{label: 'Left', id: 'Left'},{label: 'Center', id: 'Center'}],
+				default: 'Center',
+				id: 'Lens'
+			},
+			{
+				label: 'Pan',
+				type: 'number',
+				min: -17000,
+				max: 17000,
+				default: 0,
+				id: 'Pan'
+			},
+			{
+				label: 'Roll',
+				type: 'number',
+				min: -300,
+				max: 300,
+				default: 0,
+				id: 'Pan'
+			},
+			{
+				label: 'Tilt',
+				type: 'number',
+				min: -9000,
+				max: 9000,
+				default: 0,
+				id: 'Pan'
+			},
+			{
+				label: 'Zoom',
+				type: 'number',
+				min: 0,
+				max: 11800,
+				default: 5000,
+				id: 'Zoom'
+			}
+		]
+	}
+	actions[ActionId.VideoMatrixReset] ={
+		label: 'Video Matrix Reset',
+		options: [
+			{
+				label: 'Output',
+				type: 'number',
+				min: 1,
+				max: 3,
+				default: 1,
+				id: 'Output'
+			}
+		]
+	}
+	actions[ActionId.TriggerAutofocus] ={
+		label: 'Camera trigger auto focus',
+		options: [
+			{
+				label: 'CameraId',
+				id: 'CameraId',
+				type: 'number',
+				min: 1,
+				max: 7,
+				default: 1
+			}
+		]
+	}
+	actions[ActionId.CameraRamp] = {
+		label: 'Set camera ramp',
+		options: [
+			{
+				label: 'CameraId',
+				type: 'number',
+				min: 1,
+				max: 7,
+				default: 1,
+				id: 'CameraId'
+			},
+			{
+				label: 'Pan',
+				type: 'dropdown',
+				choices: [{label: 'Left', id: 'Left'},{label: 'Right', id: 'Right'},{label: 'Stop', id: 'Stop'}],
+				default: 'Stop',
+				id: 'Pan'
+			},
+			{
+				label: 'PanSpeed',
+				type: 'number',
+				min: 0,
+				max: 15,
+				default: 0,
+				id: 'PanSpeed'
+			},
+			{
+				label: 'Tilt',
+				type: 'dropdown',
+				choices: [{label: 'Down', id: 'Down'},{label: 'Up', id: 'Up'},{label: 'Stop', id: 'Stop'}],
+				default: 'Stop',
+				id: 'Tilt'
+			},
+			{
+				label: 'TiltSpeed',
+				type: 'number',
+				min: 0,
+				max: 15,
+				default: 0,
+				id: 'TiltSpeed'
+			},
+			{
+				label: 'Zoom',
+				type: 'dropdown',
+				choices: [{label: 'In', id: 'In'},{label: 'Out', id: 'Out'},{label: 'Stop', id: 'Stop'}],
+				default: 'Stop',
+				id: 'Zoom'
+			},
+			{
+				label: 'ZoomSpeed',
+				type: 'number',
+				min: 0,
+				max: 15,
+				default: 0,
+				id: 'ZoomSpeed'
+			},
+			{
+				label: 'Focus',
+				type: 'dropdown',
+				choices: [{label: 'Far', id: 'Far'},{label: 'Near', id: 'Near'},{label: 'Stop', id: 'Stop'}],
+				default: 'Stop',
+				id: 'Focus'
+			}
+		]
+	}
+	actions[ActionId.DTMFSend] = {
+		label: 'DTMF Send',
+		options: [
+			{
+				label: 'CallId',
+				id: 'CallId',
+				type: 'number',
+				min: 0,
+				max: 65534,
+				default: 0
+			},
+			{
+				label: 'DTMFString',
+				id: 'DTMFString',
+				type: 'textinput',
+				default: ''
+			}
+		]
+	}
+	actions[ActionId.ConferenceDoNotDisturbActivate] = {
+		label: 'Conference Do Not Disturb Activate',
+		options: [
+			{
+				label: 'Timeout',
+				id: 'Timeout',
+				type: 'number',
+				min: 1,
+				max: 1440,
+				default: 1
+			}
+		]
+	}
+	actions[ActionId.ConferenceDoNotDisturbDeActivate] = {
+		label: 'Conference Do Not Disturb deactivate',
+		options: []
+	}
+	actions[ActionId.SelfView] = {
+		label: 'Video: Self view',
+		options: [ 
+			WebexOnOffBooleanDropdown('Mode', 'Mode'),
+			WebexOnOffBooleanDropdown('FullscreenMode', 'FullscreenMode'),
+			{
+				label: 'PIP Position',
+				id: 'PIPPosition',
+				type: 'dropdown',
+				choices: [
+					{ id: 'LowerLeft', label: 'LowerLeft' },
+					{ id: 'LowerRight', label: 'LowerRight' },
+					{ id: 'UpperLeft', label: 'UpperLeft' },
+					{ id: 'UpperRight', label: 'UpperRight' }
+				],
+				default: 'LowerLeft'
+			},
+			{
+				label: 'OnMonitorRole',
+				id: 'OnMonitorRole',
+				type: 'dropdown',
+				choices: [
+					{ id: 'First', label: 'First' },
+					{ id: 'Second', label: 'Second' },
+					{ id: 'Third', label: 'Third' }
+				],
+				default: 'First'
+			}
+		]
+	}
+	actions[ActionId.OSDKeyClick] = {
+		label: 'OSD Key Click',
+		options: [RemoteKeyToPressDropdown()]
+	}
+	actions[ActionId.OSDKeyPress] = {
+		label: 'OSD Key Press',
+		options: [RemoteKeyToPressDropdown()]
+	}
+	actions[ActionId.OSDKeyRelease] = {
+		label: 'OSD Key Release',
+		options: [RemoteKeyToPressDropdown()]
 	}
 	return actions
 }
@@ -250,9 +657,11 @@ export async function HandleAction(
 				break
 			}
 			case ActionId.Disconnect: {
+				const callId = parseInt(String(opt.CallId))
+
 				command.id = '2'
 				command.method = 'xCommand/Call/Disconnect'
-				command.params = { CallId: instance.CallId }
+				command.params = { CallId: callId }
 				break
 			}
 			case ActionId.Accept: {
@@ -335,11 +744,154 @@ export async function HandleAction(
 				}
 				break
 			}
+			case ActionId.VideoMatrix: {
+				const sourceId = parseInt(String(opt.SourceId))
+
+				command.id = '126'
+				command.method = 'xCommand/Video/Matrix/Assign'
+				command.params = {
+					Layout: opt.Layout,
+					Mode: opt.Mode,
+					Output: opt.Output,
+					RemoteMain: opt.RemoteMain,
+					SourceId: sourceId
+				}
+				break
+			}
 			case ActionId.CameraPreset: {
 				const preset = parseInt(String(opt.PresetId))
-				command.id = '126'
+				command.id = '127'
 				command.method = 'xCommand/Camera/Preset/Activate'
 				command.params = { PresetId: preset }
+				break
+			}
+			case ActionId.SetMainVideoSource: {
+				const connectorId = parseInt(String(opt.ConnectorId))
+				const sourceId = parseInt(String(opt.SourceId))
+
+				command.id = '128'
+				command.method = 'xCommand/Video/Input/SetMainVideoSource'
+				command.params = {
+					ConnectorId: connectorId,
+					Layout:	opt.Layout,
+					PIPPosition: opt.PIPPosition,
+					PIPSize: opt.PIPSize,
+					SourceId: sourceId
+					}
+				break
+			}
+			case ActionId.CameraPositionSet: {
+				const cameraId = parseInt(String(opt.CameraId))
+				const focus = parseInt(String(opt.Focus))
+				const pan = parseInt(String(opt.Pan))
+				const roll = parseInt(String(opt.Roll))
+				const tilt = parseInt(String(opt.Tilt))
+				const zoom = parseInt(String(opt.Zoom))
+
+				command.id = '129'
+				command.method = 'xCommand/Camera/PositionSet'
+				command.params = {
+					CameraId: cameraId,
+					Focus: focus,
+					Lens: opt.Lens,
+					Pan: pan,
+					Roll: roll,
+					Tilt: tilt,
+					Zoom: zoom,
+					}
+				break
+			}
+			case ActionId.VideoMatrixReset: {
+				const output = parseInt(String(opt.Output))
+				command.id = '130'
+				command.method = 'xCommand/Video/Matrix/Reset'
+				command.params = { Output: output }
+				break
+			}
+			case ActionId.TriggerAutofocus: {
+				const cameraId = parseInt(String(opt.CameraId))
+				command.id = '131'
+				command.method = 'xCommand/Camera/TriggerAutofocus'
+				command.params = { CameraId: cameraId }
+				break
+			}
+			case ActionId.CameraRamp: {
+				const cameraId = parseInt(String(opt.CameraId))
+				const panSpeed = parseInt(String(opt.PanSpeed))
+				const tiltSpeed = parseInt(String(opt.TiltSpeed))
+				const zoomSpeed = parseInt(String(opt.ZoomSpeed))
+
+				command.id = '132'
+				command.method = 'xCommand/Camera/Ramp'
+				command.params = { 
+					CameraId: cameraId,
+					Pan: opt.Pan,
+					PanSpeed: panSpeed,
+					Tilt: opt.Tilt,
+					TiltSpeed: tiltSpeed,
+					Zoom: opt.Zoom,
+					ZoomSpeed: zoomSpeed,
+					Focus: opt.Focus
+				}
+				break
+			}
+			case ActionId.DTMFSend: {
+				const callId = parseInt(String(opt.CallId))
+				command.id = '133'
+				command.method = 'xCommand/Call/DTMFSend'
+				command.params = { 
+					CallId: callId,
+					DTMFString: opt.DTMFString
+				 }
+				break
+			}
+			case ActionId.ConferenceDoNotDisturbActivate: {
+				const timeout = parseInt(String(opt.Timeout))
+				command.id = '134'
+				command.method = 'xCommand/Conference/DoNotDisturb/Activate'
+				command.params = {
+					Timeout: timeout
+				}
+				break
+			}
+			case ActionId.ConferenceDoNotDisturbDeActivate: {
+				command.id = '135'
+				command.method = 'xCommand/Conference/DoNotDisturb/Deactivate'
+				break
+			}
+			case ActionId.SelfView: {
+				command.id = '136'
+				command.method = 'xCommand/Video/Selfview/Set'
+				command.params = {
+					Mode: opt.Mode,
+					FullscreenMode: opt.FullscreenMode,
+					PIPPosition: opt.PIPPosition,
+					OnMonitorRole: opt.OnMonitorRole
+				}
+				break
+			}
+			case ActionId.OSDKeyClick: {
+				command.id = '137'
+				command.method = 'xCommand/UserInterface/OSD/Key/Click'
+				command.params = {
+					Key: opt.Key
+				}
+				break
+			}
+			case ActionId.OSDKeyPress: {
+				command.id = '138'
+				command.method = 'xCommand/UserInterface/OSD/Key/Press'
+				command.params = {
+					Key: opt.Key
+				}
+				break
+			}
+			case ActionId.OSDKeyRelease: {
+				command.id = '139'
+				command.method = 'xCommand/UserInterface/OSD/Key/Release'
+				command.params = {
+					Key: opt.Key
+				}
 				break
 			}
 		}
